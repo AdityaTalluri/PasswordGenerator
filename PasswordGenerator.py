7babe2c68b95403777c7fa35a064dfd375b2de84
Aditya Talluri <adi123.talluri@gmail.com>
2025-07-04 13:08:54 +0530

    PasswordGenerator.py

---
+++ PasswordGenerator.py
@@ -0,0 +1,23 @@
+#Not really needed, but you can remove
+
+import random
+
+
+def make_password():
+    # Get a list of all possible characters
+    all_chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()'
+
+    # Choose a random length for the password
+    length = random.randint(8, 16)
+
+    # Make a password by picking random characters
+    password = ''
+    for i in range(length):
+        password += random.choice(all_chars)
+
+    return password
+
+
+# Make a password and print Your password is: ""
+
+print("Your Password is:",make_password())
